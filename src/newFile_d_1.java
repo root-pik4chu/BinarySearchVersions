face6f71d72e42ce3835c78fdc7d380ca0761cac
pikachu <sahilsaundale@gmail.com>
2023-07-07 13:25:30 +0530

    thirdCommit

--- src/newFile_d_1.java
+++ src/newFile_d_1.java
@@ -1,5 +1,6 @@
 public class newFile_d_1 {
     public static void main(String[] args) {
         System.out.println("this is new commit _2 ");
+        System.out.println("this is 3rd commit ");
     }
 }
